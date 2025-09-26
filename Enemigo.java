7d1c7813c9cb62648d53c2675e58874f9c27e079
Nenerox <andres.ps0308@gmail.com>
2025-09-26 17:37:57 -0600

    Se agrego el getter de la habilidad y metodo para usarla

--- Enemigo.java
+++ Enemigo.java
@@ -1,9 +1,16 @@
 public class Enemigo extends Personaje {
     private Habilidad habilidad;
-    
+
     public Enemigo(String nombre, int salud, int ataque, Habilidad habilidad) {
         super(nombre, salud, ataque, "!No tan rápido!", "¡Mis compañeros me vengaran!", "¡Esto fue por mis amigos!");
         this.habilidad = habilidad;
     }
+    
+    public Habilidad getHabilidad() {
+        return habilidad;
+    }
 
+    public int usarHabilidad() {
+        return habilidad.getAtaque();
+    }
 }
