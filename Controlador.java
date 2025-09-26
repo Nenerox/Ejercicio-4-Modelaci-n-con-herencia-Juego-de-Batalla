afa75dc2a7ed4403c7fa43707a1f820d58813300
Nenerox <andres.ps0308@gmail.com>
2025-09-26 17:41:23 -0600

    Se agrego el primer intento del bucle del juego funcional

--- Controlador.java
+++ Controlador.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Controlador {
     private ArrayList<String> itemsGuerrero = new ArrayList<>(Arrays.asList("Pocion","Pocion","Antidoto","Antidoto"));
     private ArrayList<String> itemsExplorador = new ArrayList<>(Arrays.asList("Pocion","Pocion","Pocion","Pocion","Antidoto","Antidoto","Antidoto","Antidoto"));
     private ArrayList<String> inventario;
+    Random random = new Random();
     Jugador jugador;
 
     public void CrearJugador(String nombre, int salud,  int ataque){
@@ -19,9 +19,49 @@ public class Controlador {
         }
     }
 
-    public void quitarItems(String item) {
-        inventario.remove(item);
+    public void usarItems(String item){
+        if (item.equalsIgnoreCase("pocion")) {
+            jugador.curar(20);
+            inventario.remove("Pocion");
+        } else if (item.equalsIgnoreCase("antidoto")) {
+            jugador.setEfecto(0);
+            jugador.setEnvenenado(false);
+            inventario.remove("Antidoto");
+        }
     }
 
+    public void atacarEnemigo(Personaje enemigo){
+        enemigo.recibirDaño(jugador.getAtaque());
+    }
 
+    public void atacarJugador(int daño){
+        jugador.recibirDaño(daño);
+    }
+
+    public List<Personaje> generarEnemigos(int cantidad) {
+        List<Personaje> enemigos = new ArrayList<>();
+        for (int i = 0; i < cantidad; i++) {
+            int tipo = random.nextInt(2); // 0 = rata, 1 = golem
+            if (tipo == 0) {
+                enemigos.add(new Ratas());
+            } else {
+                enemigos.add(new Golem());
+            }
+        }
+        return enemigos;
+    }
+    public List<Personaje> generarJefe() {
+        List<Personaje> jefe = new ArrayList<>();
+        int tipo = random.nextInt(2); // 0 = jefe rata, 1 = jefe golem
+        if (tipo == 0) {
+                jefe.add(new Jefe_Rata());
+            } else {
+                jefe.add(new Jefe_Golem());
+            }
+            return jefe;
+        }
+
+    public void enemigoUsaHabilidad(Enemigo enemigo) {
+    jugador.recibirDaño(enemigo.usarHabilidad());
+    }
 }
--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
@@ -8,6 +8,7 @@ public class main {
         boolean salir = false;
         Controlador controlador = new Controlador();
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.println("Cual es tu nombre?");
         nombre = scanner.nextLine();
@@ -22,8 +23,51 @@ public class main {
             controlador.CrearJugador(nombre, 200, 20);
             controlador.SetItems(clase);
         }
+
+        List<Personaje> bestias = controlador.generarEnemigos(1 + random.nextInt(2)); 
+        System.out.println(controlador.jugador.getMensaje_Inicio());
+        for (Personaje e : bestias) System.out.println(e.getMensaje_Inicio());
+        
         while (controlador.jugador.estaVivo() || salir == false){
+
+            for (int i = 0; i < bestias.size(); i++) {
+                if (bestias.get(i).estaVivo()) {
+                    System.out.println((i+1) + ") " + bestias.get(i).getNombre() + " (Vida: " + bestias.get(i).getSalud() + ")");
+                }
+            }
             
+            System.out.println("\nTurno de " + controlador.jugador.getNombre() + ". Vida: " + controlador.jugador.getSalud());
+            System.out.println("Elige acción: 1) Atacar  2) Pasar  3) items");
+            int opcion = scanner.nextInt();
+
+            switch(opcion) {
+                case 1: {
+                    System.out.println("Elije un enemigo para atacar");
+                    int enemigo = scanner.nextInt();
+                    controlador.atacarEnemigo(bestias.get(enemigo));
+                }
+                case 2: System.out.println("Se paso el turno");
+                case 3: controlador.usarItems(clase);
+            }
+
+            //Turno enemigos
+            for (int i = 0; i < bestias.size(); i++) {
+                if (bestias.get(i).estaVivo()) {
+                    if (random.nextBoolean()) {
+                        controlador.atacarJugador(bestias.get(i).getAtaque());
+                    } else {
+                        if (bestias.get(i).getNombre().equals("Rata")) {
+                            controlador.jugador.setEfecto(3);
+                            controlador.jugador.setEnvenenado(true);
+                        }
+                        controlador.enemigoUsaHabilidad((Enemigo) bestias.get(i));
+                    }
+                }
+            }
+
+            if (controlador.jugador.getEnvenenado()) {
+                controlador.atacarJugador(controlador.jugador.getEfecto());
+            }
         }
         System.out.println("Te mataron, recuerda que aveces la recompensa no vale el riesgo.");
     }
