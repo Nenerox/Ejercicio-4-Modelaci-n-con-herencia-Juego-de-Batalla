f73a74e6d96dee1794b1590159f4f7151ef7094a
Nenerox <andres.ps0308@gmail.com>
2025-09-26 17:40:23 -0600

    Se corrigio texto

--- Ratas.java
+++ Ratas.java
@@ -1,5 +1,5 @@
 public class Ratas extends Enemigo {
     public Ratas() {
-        super("Ratas", 50, 5, new Habilidad("Mordisco Venenoso", 3, "Un ataque debil pero que infecta."));
+        super("Rata", 50, 5, new Habilidad("Mordisco Venenoso", 3, "Un ataque debil pero que infecta."));
     }
 }
