4b842169de6f98c78d7d4452bd84057c62a6f92e
Nenerox <andres.ps0308@gmail.com>
2025-09-26 12:57:45 -0600

    Se empezo el ingreso de nombre y creacion de personajes con sus items

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Controlador {
-    
+    private ArrayList<String> itemsGuerrero = new ArrayList<>(Arrays.asList("Pocion","Pocion","Antidoto","Antidoto"));
+    private ArrayList<String> itemsExplorador = new ArrayList<>(Arrays.asList("Pocion","Pocion","Pocion","Pocion","Antidoto","Antidoto","Antidoto","Antidoto"));
+    private ArrayList<String> inventario;
+    Jugador jugador;
+
+    public void CrearJugador(String nombre, int salud,  int ataque){
+        this.jugador = new Jugador(nombre, salud, ataque);
+    }
+
+    public void SetItems(String clase){
+        if (clase.equalsIgnoreCase("guerrero")) {
+            this.inventario = itemsGuerrero;
+        } else {
+            this.inventario = itemsExplorador;
+        }
+    }
+
+    public void quitarItems(String item) {
+        inventario.remove(item);
+    }
+
+
 }
--- main.java
+++ main.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        String nombre;
+        String clase;
+        boolean salir = false;
         Controlador controlador = new Controlador();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Cual es tu nombre?");
+        nombre = scanner.nextLine();
+
+        System.out.println("Cual es tu clase? (Guerrero/Explorador)");
+        clase = scanner.nextLine();
+
+        if (clase.equalsIgnoreCase("guerrero")) {
+            controlador.CrearJugador(nombre, 300, 40);
+            controlador.SetItems(clase);
+        } else {
+            controlador.CrearJugador(nombre, 200, 20);
+            controlador.SetItems(clase);
+        }
+        while (controlador.jugador.estaVivo() || salir == false){
+            
+        }
+        System.out.println("Te mataron, recuerda que aveces la recompensa no vale el riesgo.");
     }
 }
